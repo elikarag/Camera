27a7acc459e8be392cc11b1fa9cba08ce1a32d87
elikarag <Elissaios.Karageorgiou@ucsf.edu>
2015-11-08 11:14:32 -0800

    Homework 2 done. into4D has both evaluation of slice variance and difference between tasks.  Also fMRI_homework_...py has the upload of an anatomical image.

--- python/camera.py
+++ python/camera.py
@@ -4,9 +4,10 @@ Created on Mon Nov  2 15:33:04 2015
 
 @author: Elissaios
 """
-
+#%%
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 #folder = 'c:\Users\Elissaios\Box Sync\Projects\fMRI\analysis1'
 folder = r"c:\Users\Elissaios\Box Sync\Projects\fMRI\analysis1";
@@ -29,5 +30,6 @@ my2d = np.rot90(my2d,3)
 my2dBW = my2d.copy()
 my2dBW[my2dBW>0.1] = 1
 my2dBW[my2dBW<=0.1] = 0
-plt.imshow(my2d)
-#plt.imshow(my2dBW,'gray')
+#plt.imshow(my2d)
+plt.imshow(my2dBW,'gray')
+plt.show()
